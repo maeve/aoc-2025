49017d5bdc926ea306e1f3b7cb27f3c9fe80c22e
Maeve Revels <maeve@aha.io>
2025-12-01 23:02:42 -1000

    Day 2 Part 2

--- 02/part2.rb
+++ 02/part2.rb
@@ -3,6 +3,25 @@
 
 input = File.readlines('./input.txt').map(&:chomp)
 
-# Solution here
+results = []
 
-puts "Answer: #{result}"
+input.each do |line|
+  line.split(',').each do |range|
+    start, finish = range.split('-')
+    invalid_ids = []
+
+    (start..finish).each do |id|
+      invalid_ids << id if (id + id)[1...-1].include?(id)
+    end
+
+    if invalid_ids.empty?
+      puts "- #{range} contains no invalid IDs."
+    else
+      puts "- #{range} has #{invalid_ids.size} invalids IDs, #{invalid_ids.join(' and ')}."
+    end
+
+    results += invalid_ids
+  end
+end
+
+puts "Answer: #{results.map(&:to_i).sum}"
