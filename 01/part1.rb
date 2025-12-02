5784f66c28d2f945bf568c9504cdccbae0c235fc
Maeve Revels <maeve@aha.io>
2025-12-01 22:02:43 -1000

    Day 1 Part 1

--- 01/part1.rb
+++ 01/part1.rb
@@ -3,6 +3,21 @@
 
 input = File.readlines('./input.txt').map(&:chomp)
 
-# Solution here
+result = 0
+current = 50
+
+puts "The dial starts by pointing at #{current}."
+
+input.each do |rotation|
+  direction = rotation[0]
+  clicks = rotation[1..].to_i
+
+  clicks = -clicks if direction == 'L'
+  current = (current + clicks) % 100
+
+  puts "The dial is rotated #{rotation} to point at #{current}."
+
+  result += 1 if current.zero?
+end
 
 puts "Answer: #{result}"
