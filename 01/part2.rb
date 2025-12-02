6ecfdc2e1af515245e659aa9a329859ec122094b
Maeve Revels <maeve@aha.io>
2025-12-01 22:09:35 -1000

    Day 1 Part 2

--- 01/part2.rb
+++ 01/part2.rb
@@ -3,6 +3,18 @@
 
 input = File.readlines('./input.txt').map(&:chomp)
 
-# Solution here
+result = 0
+current = 50
+
+input.each do |rotation|
+  direction = rotation[0]
+  clicks = rotation[1..].to_i
+
+  clicks = -clicks if direction == 'L'
+  count = (current + clicks) / 100
+  current = (current + clicks) % 100
+
+  result += count.abs
+end
 
 puts "Answer: #{result}"
