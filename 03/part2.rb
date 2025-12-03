2d8c2701cc30b548603d14a146d225aba2b72273
Maeve Revels <maeve@aha.io>
2025-12-03 12:11:14 -1000

    Day 3 Part 2

--- 03/part2.rb
+++ 03/part2.rb
@@ -3,6 +3,19 @@
 
 input = File.readlines('./input.txt').map(&:chomp)
 
-# Solution here
+joltages = []
 
-puts "Answer: #{result}"
+input.each do |line|
+  bank = line.chars.map(&:to_i)
+  digits = []
+
+  until digits.size == 12
+    initial_index = bank.index(digits.last) || -1
+    bank = bank.slice((initial_index + 1)..)
+    digits << bank[0..-(12 - digits.size)].max
+  end
+
+  joltages << digits.map(&:to_s).join.to_i
+end
+
+puts "Answer: #{joltages.sum}"
