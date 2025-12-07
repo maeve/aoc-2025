d425b062ea831d1c23a3dcd60d1dee4ef2826a9f
Maeve Revels <maeve@aha.io>
2025-12-06 22:27:20 -1000

    Day 6 Part 2

--- 06/part2.rb
+++ 06/part2.rb
@@ -3,6 +3,32 @@
 
 lines = File.readlines('./input.txt').map(&:chomp)
 
-# TODO: Implement solution
+result = 0
+
+operator_input = lines.pop
+
+grid = lines.map(&:chars)
+
+until operator_input.empty?
+  operator = operator_input.match(/^[^ ] */)[0]
+  operator_input.sub!(operator, '')
+
+  operand_inputs = []
+
+  grid.each do |line|
+    operand_inputs << line.shift(operator.length)
+  end
+
+  operands = []
+
+  operand_inputs.transpose.each do |row|
+    value = row.join.strip
+    next if value.empty?
+
+    operands << value.to_i
+  end
+
+  result += operands.inject(operator.strip.to_sym)
+end
 
 puts "Answer: #{result}"
